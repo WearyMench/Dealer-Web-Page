13c6cc04ae249a6e42afdb5165a79207a8b88e7f
Adrian Mirabal <adrian.mirabal07@gmail.com>
2025-04-14 16:50:41 -0400

    fix

--- js/cotizar.js
+++ js/cotizar.js
@@ -5,7 +5,7 @@ burger.addEventListener("click", () => {
 });
 
 function calcularPrecio() {
-  const basePath = window.location.pathname.includes(".github.io")
+  const basePath = window.location.pathname.includes("/Dealer-Web-Page/")
     ? "/Dealer-Web-Page/"
     : "/";
   console.log(basePath);
